1a4c528438383786c47182b15e54ff8b0c8d6558
Mary Selig <seligmar@gmail.com>
2019-02-15 19:51:53 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p'); 
+}
+
+function lastImageSelector() {
+  return $('ul li:first-child'); 
+}
+
+function ninjaBabySelector() {
+  return $("#baby-ninja"); 
+}
+
+function divSelector() {
+  return $('.pics'); 
+}
+
+function firstListItem() {
+  return $("#pic-list: ul:first-child")
+}
